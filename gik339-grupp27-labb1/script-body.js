30fc6a8477f3de2267cb03eda4643d7f1925510d
Akkely <112615486+Akkely@users.noreply.github.com>
2023-11-17 12:12:03 +0100

    17/11

--- gik339-grupp27-labb1/script-body.js
+++ gik339-grupp27-labb1/script-body.js
@@ -1,64 +1,67 @@
-/* Hämtar div textbox med hjälp av getElementsByClassName, där classname är textbox */
-const textBox = document.getElementsByClassName("container");
+// Hämtar alla input-element med klassen 'textfield'
+const inputFields = document.querySelectorAll('.textfield');
 
-/* Hämtar input med hjälp av getElementById, där id är color */
-const color = document.getElementById("color");
+// Hämtar elementet där nya element ska visas
+const dynamicDisplay = document.getElementById('dynamicDisplay');
 
-/* Hämtar textfält med hjälp getElementByClassName, där className är textfield*/
-const textfield = document.getElementsByClassName("textfieldOne");
+// Hämtar input-fält för färg
+const colorField = document.getElementById('color');
 
-/* Hämtar content med hjälp av getElementById, där id är content*/
-const content = document.getElementById("content");
+// Hämtar input-fält för innehåll
+const contentField = document.getElementById('content');
+
+// Hämtar checkbox-elementet
+const divStyleCheckbox = document.querySelector('input[type="checkbox"]');
 
 
+// Kollar i consol loggen vad vi hämtat
+console.log(inputFields, dynamicDisplay, colorField, contentField,divStyleCheckbox)
 
-/* eventLyssnare(); */
 
-/* Hämtar textfält med hjälp av querySelector, där className är textfield*/
-const textfield1 = document.querySelector(".textfieldTwo");
+// Funktionen som hanterar eventet
+function handleInputEvent(e) {
+  // Skriver ut vilket element som genererade eventet
+  console.log('Event genererat av:', e.target);
+  // Hämtar name-attributet från elementet som genererade eventet
+  const fieldName = e.target.name;
+  // Kontrollerar om det är 'content'-fältet
+  if (fieldName === 'content') {
+      // Hämtar värdet från 'content'-fältet
+      const contentValue = e.target.value;
+      // Skriver ut värdet i 'content'-fältet till div-elementet
+      dynamicDisplay.innerHTML = contentValue;
+  }
+}
 
-/* Hämtar divStyle med hjälp av getElementById, där id är divStyle*/
-const divStyle = document.getElementById("divStyle");
+// Lägger till eventlyssnare till alla input-fält
+inputFields.forEach(field => field.addEventListener('input', handleInputEvent));
 
-/* Hämtar checkbox med hjälp av getElementByClassName, där className är checkbox*/
-const checkbox = document.getElementsByClassName("checkBox");
+// Lägger till en eventlyssnare på checkboxen för att hantera förändringar
+divStyleCheckbox.addEventListener('change', function() {
+  // Kontrollerar om checkboxen är ikryssad
+  if (this.checked) {
+    // Rensar befintligt innehåll i dynamicDisplay
+    dynamicDisplay.innerHTML = '';
+    // Skapar ett nytt div-element
+    const newElement = document.createElement('div');
+    // Lägger till en klass till det nya elementet
+    newElement.classList.add('new-element');
+    // Sätter bakgrundsfärgen på det nya elementet baserat på värdet i colorField
+    newElement.style.backgroundColor = colorField.value;
+    // Sätter innehållet i det nya elementet baserat på värdet i contentField
+    newElement.innerHTML = contentField.value;
+    // Lägger till det nya elementet i dynamicDisplay
+    dynamicDisplay.appendChild(newElement);
+  }
+});
 
-/* Hämtar knappen med hjälp av getElementByTagName, där tagName är delete*/
-const button = document.querySelector("#delete");
-
-/* Skapar en funktion (eventlyssnare) som när den triggas 
-ska avsändaren (target) skrivas ut till konsolen. Om avsändaren (target) 
-är inputfältet content, ska dess värde skrivas ut till div-elementet */
-
-/* getTextfield1.addEventListener'click', () => {
-    content = document.getElementsByName("content");
-    content.for((content) => {
-        if (content.checked) {
-            output.innerText = `You selected: ${content.value}`;
-        }
-    }); */
-
-/* function eventLyssnare(event) {*/
-    /* alert("Target")
-    content.addEventListener("click", eventLyssnare); */
-    /* let e = event || window.event; */
-/* Skriver ut till konsolen vilket inputfält som genererade eventet (target) */
-   /*  console.log("Event triggered by: " + e.content.id);
- */
-/* Tar reda på inputfältets name attribut*/
-   /*  let fieldName = e.target.name;
- */
-    /* Om name-attributet är "content" */
-  /*   if (fieldName == "content") {
-        let outputDiv = document.getElementById("output"); */
-
-        /* Skriver ut värdet i inputfältet till div-elementet */
-       /*  outputDiv.innerHTML = "Input value: " + e.content.value;
-    }
-}  */
-
-/* Hämtar input-elementet */
-/* const contentInput = document.getElementById("content"); */
-
-/* Lägger till en eventlyssnare till input-elementet */
-/* contentInput.addEventListener("input", handleInput); */
+// Hämtar "Ta bort"-knappen
+const deleteButton = document.querySelector("#delete");
+console.log(deleteButton)
+// Lägger till en eventlyssnare på "Ta bort"-knappen
+deleteButton.addEventListener('click', function() {
+    // Hämtar dynamicDisplay-elementet
+    const dynamicDisplay = document.getElementById('dynamicDisplay');
+    // Rensar allt innehåll i dynamicDisplay när knappen klickas
+    dynamicDisplay.innerHTML = '';
+});
