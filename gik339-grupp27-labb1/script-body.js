285c66e49ce20fc86333db173b4294bc4c5193b2
Akkely <112615486+Akkely@users.noreply.github.com>
2023-12-08 08:13:31 +0100

    labb 1

--- gik339-grupp27-labb1/script-body.js
+++ gik339-grupp27-labb1/script-body.js
@@ -1,31 +1,57 @@
-/* Hämtar div textbox med hjälp av getElementsByClassName, där classname är textbox */
-const textBox = document.getElementsByClassName("container");
+// /* Hämtar div textbox med hjälp av getElementsByClassName, där classname är textbox */
+// const textBox = document.getElementsByClassName("container");
 
-/* Hämtar input med hjälp av getElementById, där id är color */
-const color = document.getElementById("color");
+// /* Hämtar input med hjälp av getElementById, där id är color */
+// const color = document.getElementById("color");
 
-/* Hämtar textfält med hjälp getElementByClassName, där className är textfield*/
-const textfield = document.getElementsByClassName("textfieldOne");
+// /* Hämtar textfält med hjälp getElementByClassName, där className är textfield*/
+// const textfield = document.getElementsByClassName("textfieldOne");
 
-/* Hämtar content med hjälp av getElementById, där id är content*/
-const content = document.getElementById("content");
+// /* Hämtar content med hjälp av getElementById, där id är content*/
+// const content = document.getElementById("content");
 
 
 
-/* eventLyssnare(); */
+// /* eventLyssnare(); */
 
-/* Hämtar textfält med hjälp av querySelector, där className är textfield*/
-const textfield1 = document.querySelector(".textfieldTwo");
+// /* Hämtar textfält med hjälp av querySelector, där className är textfield*/
+// const textfield1 = document.querySelector(".textfieldTwo");
 
-/* Hämtar divStyle med hjälp av getElementById, där id är divStyle*/
-const divStyle = document.getElementById("divStyle");
+// /* Hämtar divStyle med hjälp av getElementById, där id är divStyle*/
+// const divStyle = document.getElementById("divStyle");
 
-/* Hämtar checkbox med hjälp av getElementByClassName, där className är checkbox*/
-const checkbox = document.getElementsByClassName("checkBox");
+// /* Hämtar checkbox med hjälp av getElementByClassName, där className är checkbox*/
+// const checkbox = document.getElementsByClassName("checkBox");
 
-/* Hämtar knappen med hjälp av getElementByTagName, där tagName är delete*/
-const button = document.querySelector("#delete");
+// /* Hämtar knappen med hjälp av getElementByTagName, där tagName är delete*/
+// const button = document.querySelector("#delete");
 
+
+function handleBlur(e){
+  const name = e.target.name;
+  const value = e.target.value;
+ // output.innerHTML= '<p> Fältet ' + name + ' har värdet ' + value + '</p>';
+ const html = `<p> Fältet ${name} har värdet ${value}</p>`;
+ output.insertAdjacentHTML('afterbegin', html);
+}
+const dynamicDisplay = document.getElementById('dynamicDisplay');
+function handleClick(e){
+  e.preventDefault();
+  const targetName = e.target.id;
+  const colorField = document.getElementById('color');
+  const contentField = document.getElementById('content');
+  //const colorField = settings.color;
+  //const contentField = setting.content;
+  console.log(targetName);
+  if(targetName == 'addElement'){
+    const newElement = document.createElement('div');
+    newElement.classList.add('new-element');
+    newElement.style.backgroundColor=colorField.value;
+    newElement.innerHTML = contentField.value;
+    dynamicDisplay.insertAdjacentElement('beforeend', newElement);
+  }
+}
+document.addEventListener('reset', () => console.log('reset'));
 /* Skapar en funktion (eventlyssnare) som när den triggas 
 ska avsändaren (target) skrivas ut till konsolen. Om avsändaren (target) 
 är inputfältet content, ska dess värde skrivas ut till div-elementet */
